8bd9ad7f49c30f857ad41eda1807421d0b0dfe66
BalandinaM <marina.balandina.89@mail.ru>
2026-05-07 15:49:12 +0500

    task 2 render DeckItem

--- src/features/decks/DecksList/DeckItem/DeckItem.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -1,7 +1,8 @@
+import { Deck } from '../../decksApi.types'
 import s from './DeckItem.module.css'
 
 type DeckProps = {
-  deck: any // todo: fix
+  deck: Deck // todo: fix
 }
 
 const TEST_ACC_NAME = 'kukus'
--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -3,12 +3,21 @@ import s from './DecksList.module.css'
 import { decksApi } from '../decks-api'
 import { setDecksAC } from '../decks-reducer'
 import { useAppDispatch, useAppSelector } from '../../../app/store'
+import { DeckItem } from './DeckItem/DeckItem'
 
 export const DecksList = () => {
   const decks = useAppSelector((state) => state.decksReducer.decks)
   const dispatch = useAppDispatch()
+
   useEffect(() => {
     decksApi.getDecks().then((res) => dispatch(setDecksAC(res.data.items)))
   }, [dispatch])
-  return <ul className={s.list}></ul>
+
+  return (
+    <ul className={s.list}>
+      {decks.map((deck) => {
+        return <DeckItem deck={deck} key={deck.id} />
+      })}
+    </ul>
+  )
 }
